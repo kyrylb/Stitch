706f33db6d163eab77d79b66b3c549d1b1c5ec13
kyrylb <kyrylb@users.noreply.github.com>
2016-05-10 02:34:53 +0300

    Bootstrap sources added; logging added; login added

--- social_net/urls.py
+++ social_net/urls.py
@@ -7,4 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^users/', views.user_list, name='user_list'),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Only for debug MODE!
+    #url( r'^accounts/signup/$', signup ),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^registration/profile/$', views.profile, name='profile'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Only for debug MODE!
--- social_net/views.py
+++ social_net/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import User, Subscription
+import logging
 
 # Create your views here.
+
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('simpleLogger')
 
 
 def user_list(request):
@@ -11,12 +16,23 @@ def user_list(request):
     :param request:
     :return: list of profiles with topic information and subscription status for current user
     """
+    logging.debug('Request cookies: %s', request.COOKIES)
+    logging.debug('Request session: %s', request.session)
+
     users = User.objects.filter(verified=True).order_by('last_name', 'name')
+    logging.debug('Users: %s', users)
     #current_user = request.user
     #subscription = Subscription.objects.filter(profile=current_user.profile)
     subscription = Subscription.objects.all()
-    return render(request, 'user_list.html', {'users': users, 'subscription': subscription})
+    logging.debug('Subscription: %s', subscription)
+
+    response = render(request, 'user_list.html', {'users': users, 'subscription': subscription})
+    logging.debug('Response: %s', response)
+    return response
 
 
 def home(request):
     return render(request, 'index.html', {})
+
+def profile(request):
+    return render(request, 'registration/profile.html', {})
